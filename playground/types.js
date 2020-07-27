fd8ebb53bb476754f4a57b0f31c9777f1f1761eb
dmoftakhar <david@chillaweb.com>
2020-07-27 19:07:41 +0100

    Number complete.

--- playground/types.js
+++ playground/types.js
@@ -1,22 +1,29 @@
 /* eslint-disable */
-const name = "David";
-const middle = 'mo';
-const last = `Mofty`;
+// const name = "David";
+// const middle = 'mo';
+// const last = `Mofty`;
 
-const sentence = 'She\'s so cool';
-const sentenceTwo = `She's so "cool"
-I'll probably write
-A song
-`;
+// const sentence = 'She\'s so cool';
+// const sentenceTwo = `She's so "cool"
+// I'll probably write
+// A song
+// `;
 
-const sentenceFull = `hello my name is ${name}
-I am ${1 + 44}
-years old
-`;
+// const sentenceFull = `hello my name is ${name}
+// I am ${1 + 44}
+// years old
+// `;
 
-const html = `
-<div>
-  <h2>${name}</h2>
-  <p>${sentenceFull}</p>
-</div>
-`;
+// const html = `
+// <div>
+//   <h2>${name}</h2>
+//   <p>${sentenceFull}</p>
+// </div>
+// `;
+
+// const nae = "David";
+// const age = 22.5;
+const smarties = 20;
+const kids = 3;
+const response = `Each kid gets ${Math.ceil(smarties / kids)} smarties`;
+console.log(response);
