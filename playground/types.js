665b99b959279e3f8a8b65d684bb8434e0850fd3
dmoftakhar <david@chillaweb.com>
2020-07-27 18:53:18 +0100

    Strings completed.

--- playground/types.js
+++ playground/types.js
@@ -2,3 +2,21 @@
 const name = "David";
 const middle = 'mo';
 const last = `Mofty`;
+
+const sentence = 'She\'s so cool';
+const sentenceTwo = `She's so "cool"
+I'll probably write
+A song
+`;
+
+const sentenceFull = `hello my name is ${name}
+I am ${1 + 44}
+years old
+`;
+
+const html = `
+<div>
+  <h2>${name}</h2>
+  <p>${sentenceFull}</p>
+</div>
+`;
