c7320ea92f40e13039ed366dcabae75209ad37b9
dmoftakhar <david@chillaweb.com>
2020-08-16 17:04:06 +0100

    Selecting elements in the DOM.

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+// console.log('linked!!');
+
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+console.log(item2);
+
+ 
