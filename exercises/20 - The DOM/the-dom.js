a4c48e74cef056902ed21bccde39726c7b5c18e3
dmoftakhar <david@chillaweb.com>
2020-08-17 18:59:07 +0100

    Element properties and methods.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,8 +1,24 @@
 // console.log('linked!!');
 
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-console.log(item2);
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// console.log(item2);
 
- 
+const heading = document.querySelector('h2');
+console.log(heading.textContent);
+console.log(heading.innerText);
+// heading.textContent = "David is cool";
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+// console.dir(heading.textContent);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList =  document.querySelector('.pizza');
+
+// pizzaList.textContent = `${pizzaList.textContent} pizza`;
+pizzaList.insertAdjacentText('afterbegin', 'pizza pizza');
+pizzaList.insertAdjacentText('beforeend', 'pizza pizza');
+console.log(pizzaList.textContent);
