8ebd0dffe34a5ace15e48a7bcd6c95642886ea25
dmoftakhar <david@chillaweb.com>
2020-08-23 12:11:03 +0100

    Built in and custom data attributes.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,24 +1,60 @@
-// console.log('linked!!');
+// // console.log('linked!!');
 
-// const p = document.querySelector('p');
-// const imgs = document.querySelectorAll('.item img');
-// const item2 = document.querySelector('.item2');
-// console.log(item2);
+// // const p = document.querySelector('p');
+// // const imgs = document.querySelectorAll('.item img');
+// // const item2 = document.querySelector('.item2');
+// // console.log(item2);
 
-const heading = document.querySelector('h2');
-console.log(heading.textContent);
-console.log(heading.innerText);
-// heading.textContent = "David is cool";
+// const heading = document.querySelector('h2');
 // console.log(heading.textContent);
 // console.log(heading.innerText);
-// console.dir(heading.textContent);
+// // heading.textContent = "David is cool";
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
+// // console.dir(heading.textContent);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList =  document.querySelector('.pizza');
+// const pizzaList =  document.querySelector('.pizza');
 
-// pizzaList.textContent = `${pizzaList.textContent} pizza`;
-pizzaList.insertAdjacentText('afterbegin', 'pizza pizza');
-pizzaList.insertAdjacentText('beforeend', 'pizza pizza');
-console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent} pizza`;
+// pizzaList.insertAdjacentText('afterbegin', 'pizza pizza');
+// pizzaList.insertAdjacentText('beforeend', 'pizza pizza');
+// console.log(pizzaList.textContent);
+
+// Classes!
+
+const nicePic = document.querySelector(".nice");
+nicePic.classList.add('dog');
+// nicePic.classList.remove('round');
+nicePic.classList.toggle('round');
+console.log(nicePic.classList);
+
+function toggleRound() {
+    nicePic.classList.toggle('round');
+}
+
+nicePic.addEventListener('click', toggleRound);
+
+// nicePic.width = 930;
+nicePic.alt = 'Cute Puppy'; // setter
+console.log(nicePic.alt); // getter
+console.log(nicePic.naturalWidth); // getter
+
+// nicePic.addEventListener('load', function(){
+//     console.log(nicePic.naturalWidth); // getter
+// });
+
+nicePic.setAttribute('alt', 'Really cute pup');
+nicePic.setAttribute('test', 'Really cute test');
+
+console.log(nicePic.getAttribute('alt'));
+
+const custom = document.querySelector('.custom');
+console.log(custom.dataset);
+
+custom.addEventListener('click', function() {
+    // console.log('you clicked on the custom attribute');
+    alert(`welcome ${custom.dataset.name} and last name ${custom.dataset.last}`);
+})
