96ce837ce3f66720c5d9898820398f8e70663cae
dmoftakhar <david@chillaweb.com>
2020-09-27 13:21:28 +0100

    Scroll events and intersection observer.

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,31 @@
+
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+
+function obCallback(payload) {
+    if((payload[0].intersectionRatio) === 1){
+        button.disabled = false;
+        console.log('removing disabled');
+    } else {
+        button.disabled = true;
+        // stop observing button
+        ob.unobserve(terms.lastElementChild);
+    }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
+
+// ob.observe(watch);
+
+// terms.addEventListener('scroll', function(e){
+//     console.log(e.currentTarget.scrollTop);
+//     console.log(e.currentTarget.scrollHeight);
+// });
+
