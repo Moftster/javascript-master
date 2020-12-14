320d0f647b4e3de34c60b36d83e557c4ad839218
DavidMoftakhar <david@chillaweb.com>
2020-12-14 09:25:30 +0000

    Random dad joke generator.

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,7 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +12,35 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke(){
+  loader.classList.remove('hidden');
+  jokeButton.classList.add('hidden');
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    }
+  });
+  const data = await response.json(); 
+  loader.classList.add('hidden');
+  jokeButton.classList.remove('hidden');
+  return data;
+}
+
+function randomItemFromArray(array, not){
+  const item = array[Math.floor(Math.random() * array.length)];
+  if(item === not){
+    console.log('We used that one last time, please try again');
+    return randomItemFromArray(array, not);
+  }
+  return item;
+}
+
+async function handleClick(){
+  const {joke} = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
+
